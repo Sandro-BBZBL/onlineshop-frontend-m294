db11d816edd1179623ea59647e72509809fca719
Sandro Bucher <sandro.bucher@bbzbl-it.ch>
2026-06-12 08:45:30 +0200

    order history fix

--- src/app/components/order-table/order-table.component.ts
+++ src/app/components/order-table/order-table.component.ts
@@ -1,34 +1,45 @@
-import { Component, Input, ViewChild, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
+import { CommonModule, DecimalPipe, DatePipe } from '@angular/common';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { Order } from '../../dataaccess/order';
 
 @Component({
   selector: 'app-order-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, MatPaginatorModule],
-  templateUrl: 'order-table.component.html',
-  styleUrls: ['./order-table.component.scss']
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    DecimalPipe,
+    DatePipe
+  ],
+  templateUrl: './order-table.component.html'
 })
-export class OrderTableComponent implements AfterViewInit, OnChanges {
-  // Nimmt die Bestellungen von jeder beliebigen Page (User oder Admin) entgegen
-  @Input({ required: true }) orders: Order[] = [];
+export class OrderTableComponent implements AfterViewInit {
+  // Nimmt die Daten von außen (der History Page) entgegen
+  @Input() set orders(data: Order[]) {
+    this.dataSource.data = data;
+  }
 
-  columns = ['id', 'orderDate', 'totalPrice'];
-  orderDataSource = new MatTableDataSource<Order>();
+  // Steuert, ob wir die Spalte "Kunde" (benutzername) anzeigen
+  @Input() isAdmin: boolean = false; 
+
+  public dataSource = new MatTableDataSource<Order>();
 
   @ViewChild(MatPaginator) paginator?: MatPaginator;
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['orders']) {
-      this.orderDataSource.data = this.orders;
+  // Bestimmt dynamisch, welche Spalten gezeichnet werden
+  get columns(): string[] {
+    if (this.isAdmin) {
+      return ['id', 'benutzername', 'orderDate', 'totalPrice'];
     }
+    return ['id', 'orderDate', 'totalPrice'];
   }
 
   ngAfterViewInit() {
     if (this.paginator) {
-      this.orderDataSource.paginator = this.paginator;
+      this.dataSource.paginator = this.paginator;
     }
   }
 }
--- src/app/pages/order-history/order-history.component.ts
+++ src/app/pages/order-history/order-history.component.ts
@@ -1,19 +1,12 @@
-import { AfterViewInit, Component, OnInit, ViewChild, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core'; 
 import { HeaderService } from '../../service/header.service';
 import { OrderService } from '../../service/order.service'; 
 import { AppAuthService } from '../../service/app.auth.service'; 
 import { Order } from '../../dataaccess/order'; 
-import { MatTableDataSource, MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 import { BaseComponent } from '../../components/base/base.component';
-import { IsInRoleDirective } from '../../dir/is.in.role.dir';
-import { MatToolbar } from '@angular/material/toolbar';
-import { MatButton } from '@angular/material/button';
-import { MatIcon } from '@angular/material/icon';
-import { DecimalPipe, DatePipe, CommonModule } from '@angular/common';
+import { OrderTableComponent } from '../../components/order-table/order-table.component';
 
 @Component({
   selector: 'app-order-history',
@@ -22,79 +15,72 @@ import { DecimalPipe, DatePipe, CommonModule } from '@angular/common';
   styleUrls: ['./order-history.component.scss'],
   imports: [
     CommonModule,
-    IsInRoleDirective, 
-    MatToolbar, 
-    MatButton, 
-    MatIcon, 
-    MatTable, 
-    MatColumnDef, 
-    MatHeaderCellDef, 
-    MatHeaderCell, 
-    MatCellDef, 
-    MatCell, 
-    MatHeaderRowDef, 
-    MatHeaderRow, 
-    MatRowDef, 
-    MatRow, 
-    MatPaginator, 
-    DecimalPipe, 
-    DatePipe, 
-    TranslateModule
+    OrderTableComponent
   ]
 })
-export class OrderHistoryComponent extends BaseComponent implements OnInit, AfterViewInit {
+export class OrderHistoryComponent extends BaseComponent implements OnInit {
   private headerService = inject(HeaderService);
   private orderService = inject(OrderService); 
   private authService = inject(AppAuthService); 
   private snackBar = inject(MatSnackBar);
-  private router = inject(Router);
+  private cdr = inject(ChangeDetectorRef);
 
-  columns = ['id', 'orderDate', 'totalPrice'];
-  orderDataSource = new MatTableDataSource<Order>();
-  
-  @ViewChild(MatPaginator) paginator?: MatPaginator;
+  public loadedOrders: Order[] = [];
+  public isAdmin: boolean = false;
 
   public constructor() {
     super();
     this.headerService.setPage('nav.order_history');
   }
 
-  async ngOnInit() {
-    await this.reloadData();
+  ngOnInit() {
+    this.checkRoleAndLoadData();
   }
 
-  ngAfterViewInit() {
-    if (this.paginator) {
-      this.orderDataSource.paginator = this.paginator;
-    }
-  }
+  private checkRoleAndLoadData() {
+    this.authService.getRoles().subscribe({
+      next: (roles: string[]) => {
+        this.isAdmin = roles.some(role => role.toLowerCase() === 'admin'); 
+        
+        if (this.isAdmin) {
+          this.orderService.getList().subscribe({
+            next: (orders) => {
+              // Sortiert alle Bestellungen absteigend nach ID (neueste zuerst)
+              this.loadedOrders = orders.sort((a, b) => b.id - a.id);
+              this.cdr.detectChanges(); 
+            },
+            error: (err) => {
+              this.snackBar.open('Fehler beim Laden aller Bestellungen.', 'Schließen', { duration: 4000 });
+            }
+          });
+        } else {
+          let username = 'unbekannt';
+          const claims = this.authService.getIdentityClaims();
+          
+          if (claims && claims['preferred_username']) {
+            username = claims['preferred_username'];
+          }
 
-  async reloadData() {
-    let username = 'unbekannt';
-    
-    const claims = this.authService.getIdentityClaims();
-    if (claims && claims['preferred_username']) {
-      username = claims['preferred_username'];
-    }
+          if (username === 'unbekannt') {
+            this.snackBar.open('Benutzer nicht identifiziert.', 'Schließen', { duration: 4000 });
+            return;
+          }
 
-    if (username === 'unbekannt') {
-      this.snackBar.open('Benutzer nicht identifiziert. Verlauf nicht ladbar.', 'Schließen', { duration: 4000 });
-      return;
-    }
-
-    this.orderService.getOrdersByUsername(username).subscribe({
-      next: (orders) => {
-        console.log('Backend-Antwort für User "' + username + '":', orders);
-        this.orderDataSource.data = orders;
+          this.orderService.getOrdersByUsername(username).subscribe({
+            next: (orders) => {
+              // Sortiert die User-Bestellungen ebenfalls absteigend nach ID
+              this.loadedOrders = orders.sort((a, b) => b.id - a.id);
+              this.cdr.detectChanges(); 
+            },
+            error: (err) => {
+              this.snackBar.open('Fehler beim Laden deines Bestellverlaufs.', 'Schließen', { duration: 4000 });
+            }
+          });
+        }
       },
       error: (err) => {
-        console.error('Fehler beim Laden des Bestellverlaufs:', err);
-        this.snackBar.open('Fehler beim Laden des Bestellverlaufs.', 'Schließen', { duration: 4000 });
+        this.snackBar.open('Berechtigungen konnten nicht geprüft werden.', 'Schließen', { duration: 4000 });
       }
     });
-  }
-
-  viewDetails(order: Order): void {
-    this.router.navigate(['/orders', order.id]);
   }
 }
